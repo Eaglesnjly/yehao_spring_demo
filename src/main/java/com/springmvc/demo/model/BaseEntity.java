99b7b19ff6431f67e1924d2f09706d1efcbcb546
Eaglesnjly <1223952541@qq.com>
2017-03-27 14:01:52 +0800

    first commit

---
+++ src/main/java/com/springmvc/demo/model/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.springmvc.demo.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 基础对象，对象共有的一些属性
+ * Created by yehao on 2016/10/13.
+ */
+
+@Data
+public class BaseEntity implements Serializable {
+	private Integer id;
+	private Integer isDel;
+
+	private Integer pageNo;
+	private Integer pageSize;
+	private Integer pageIndex;
+	private String orderBy;
+
+	private Date createTime;
+	private Date updateTime;
+
+
+	public void init(){
+		if (this.pageNo ==null){
+			this.pageNo = 1;
+		}
+		if(this.pageSize == null){
+			this.pageSize = 20;
+		}
+		this.pageIndex = (this.pageNo-1)*this.pageSize;
+	}
+}
